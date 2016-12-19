ba5a12572e6f347789f63ba4fd3effb12ebcbb68
kelam <kevinlamontagne@gmail.com>
2016-12-19 15:50:35 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,18 +3,10 @@ maintainer       "Sonian, Inc."
 maintainer_email "chefs@sonian.net"
 license          "Apache 2.0"
 description      "Installs/Configures Sensu"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description ''
 version          "3.1.2"
 
 depends "apt", ">= 2.0"
 depends "rabbitmq", ">= 2.0.0"
 
-%w[
-  ubuntu
-  debian
-].each do |os|
-  supports os
-end
-
-source_url 'https://github.com/klamontagne/sensu-chef'
-chef_version '>= 12.0'
+supports 'ubuntu'
