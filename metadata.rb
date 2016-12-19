6eb76272826b254eb183986091ecbddcfc9cd489
kelam <kevinlamontagne@gmail.com>
2016-12-19 15:55:45 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures Sensu"
 long_description ''
 version          "3.1.2"
 
-depends "apt", ">= 2.0"
-depends "rabbitmq", ">= 2.0.0"
+#depends "apt", ">= 2.0"
+#depends "rabbitmq", ">= 2.0.0"
 
 supports 'ubuntu'
